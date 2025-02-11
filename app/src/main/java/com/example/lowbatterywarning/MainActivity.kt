406f6fa7dd42ccfc2825e0599f47bcc8d40dfb24
Pedrosilva03 <pedroo.silva180@gmail.com>
2025-02-11 19:48:53 +0000

    App requests the user to disable battery optimizations

--- app/src/main/java/com/example/lowbatterywarning/MainActivity.kt
+++ app/src/main/java/com/example/lowbatterywarning/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.lowbatterywarning
 
+import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.content.Context
 import android.content.IntentFilter
@@ -7,7 +8,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 
 import android.content.Intent
+import android.os.PowerManager
 import android.widget.Toast
+
+import android.net.Uri
+import android.os.Build
+import android.provider.Settings
 
 class MainActivity : ComponentActivity() {
     private lateinit var batteryReceiver: BatteryReceiver
@@ -18,6 +24,10 @@ class MainActivity : ComponentActivity() {
             Toast.makeText(this,"Enable app notifications to receive alerts (required)", Toast.LENGTH_LONG).show()
         }
 
+        askForBackgroundExecution()
+
+        createNotificationChannel()
+
         batteryReceiver = BatteryReceiver()
         val intentFilter = IntentFilter(Intent.ACTION_BATTERY_CHANGED)
         registerReceiver(batteryReceiver, intentFilter)
@@ -26,6 +36,29 @@ class MainActivity : ComponentActivity() {
     override fun onDestroy() {
         super.onDestroy()
         unregisterReceiver(batteryReceiver)
+    }
+
+    fun askForBackgroundExecution(){
+        val powerManager = getSystemService(PowerManager::class.java)
+        if (!powerManager.isIgnoringBatteryOptimizations(packageName)) {
+            val intent = Intent(Settings.ACTION_REQUEST_IGNORE_BATTERY_OPTIMIZATIONS)
+            intent.data = Uri.parse("package:$packageName")
+            startActivity(intent)
+        }
+    }
+
+    fun createNotificationChannel(){
+        val notificationManager =
+            this.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                "boot_channel",
+                "Boot notification",
+                NotificationManager.IMPORTANCE_HIGH
+            )
+            notificationManager.createNotificationChannel(channel)
+        }
     }
 }
 
